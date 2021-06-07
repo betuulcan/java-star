a8d8733fd562c2ba9470a3db39ed14977ca4dce8
betuulcan <betuulcan@hotmail.com>
2021-06-07 09:12:15 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 6; i++) {
+            for (int j = 0; j < 6 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
